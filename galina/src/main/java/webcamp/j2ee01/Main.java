9dfc2f49a7b1821b872b66c6a8941990a8b5d014
randriyanov <randriyanov@mail.ru>
2016-01-28 20:15:54 +0200

    modules added

---
+++ galina/src/main/java/webcamp/j2ee01/Main.java
@@ -0,0 +1,7 @@
+package webcamp.j2ee01;
+
+/**
+ * Created by randriyanov on 28.01.16.
+ */
+public class Main {
+}
---
+++ galina/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by randriyanov on 28.01.16.
+ */
+public class MainTest {
+
+    @Test
+    public void doSomething() {
+
+    }
+}
