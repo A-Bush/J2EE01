1d6277992fee9aac22277daf2b99b89ada7b3724
KropaM <gklymenko@gmail.com>
2016-02-01 11:26:49 +0200

    test stream

--- galina/src/main/java/webcamp/j2ee01/Main.java
+++ galina/src/main/java/webcamp/j2ee01/Main.java
@@ -4,4 +4,7 @@ package webcamp.j2ee01;
  * Created by randriyanov on 28.01.16.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("just testing here");
+    }
 }
