723035804ab7abccdb979673f6252a70fbe312f3
randriyanov <randriyanov@mail.ru>
2016-02-09 19:45:36 +0200

    commit in test

--- src/main/java/webcamp/j2ee01/TestMain.java
+++ src/main/java/webcamp/j2ee01/TestMain.java
@@ -6,6 +6,8 @@ package webcamp.j2ee01;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+
         System.out.println("Hello Roman!!!");
+
     }
 }
